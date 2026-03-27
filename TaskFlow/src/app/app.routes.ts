14db179c298caf90d0b5166ad01fde6ef70d125f
UBSITSabelaSVinChrysler <20243818@s.ubaguio.edu>
2026-03-27 12:56:25 +0800

    Fixed some bugs

--- TaskFlow/src/app/app.routes.ts
+++ TaskFlow/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { TaskAddComponent } from './task-add-component/task-add-component';
 import { TaskDetailComponent } from './task-detail-component/task-detail-component';
-import { TaskInfoComponent } from './task-info-component/task-info-component';
 import { TaskEditComponent } from './task-edit-component/task-edit-component';
 import { TaskList } from './task-list-component/task-list-component';
+import { TaskInfoComponent } from './task-info-component/task-info-component';
 
 
 
